fef824b9c0b7b98a14bec79d8c4095b8ee5bb881
tumelo <tumelom@evolvemed.co.za>
2019-10-17 10:30:40 +0200

    project solution

--- StopSpinningMyWords/StopSpinningMyWordsApp/Program.cs
+++ StopSpinningMyWords/StopSpinningMyWordsApp/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StopSpinningMyWordsApp
 {
@@ -10,6 +8,26 @@ namespace StopSpinningMyWordsApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(SpinWords("Welcome"));
+        }
+
+        static string SpinWords(string sentence)
+        {
+            string[] spaceLineSplit = new string[] { Environment.NewLine , " "};
+            string[] newSentence = sentence.Split(spaceLineSplit, StringSplitOptions.None);
+            sentence = String.Empty;
+
+            for (int i = 0; i < newSentence.Length; i++)
+            {
+                char[] charArray = newSentence[i].ToCharArray();
+                if (charArray.Length > 4)
+                {
+                    Array.Reverse(charArray);
+                }
+                sentence += $"{new string(charArray)} {string.Empty}";
+            }
+
+            return sentence.Trim();
         }
     }
 }
